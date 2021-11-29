533108016ca392dc1149bb116cdc4659f0f3eafb
Joachim Lengacher <joachim.lengacher@collaboration-factory.de>
2021-11-29 15:02:19 +0100

    Added styling for the main application

--- src/custom-endpoint.vue
+++ src/custom-endpoint.vue
@@ -1,19 +1,3 @@
-<!--
-  - Copyright 2014-2018 the original author or authors.
-  -
-  - Licensed under the Apache License, Version 2.0 (the "License");
-  - you may not use this file except in compliance with the License.
-  - You may obtain a copy of the License at
-  -
-  -     http://www.apache.org/licenses/LICENSE-2.0
-  -
-  - Unless required by applicable law or agreed to in writing, software
-  - distributed under the License is distributed on an "AS IS" BASIS,
-  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  - See the License for the specific language governing permissions and
-  - limitations under the License.
-  -->
-
 <template>
   <div class="interfaces">
     <div class="card-wrapper">
@@ -84,6 +68,129 @@
   }
   .interfaces .card .tags {
     padding: 10px 0;
+  }
+
+
+  /* TODO: these style don't really belong to this custom view. They are here to help styling the whole
+      application. We need to find another place for them.
+   */
+  .navbar {
+    background-color: #f3f4f7 !important;
+  }
+  .navbar-item {
+    color: black !important;
+  }
+
+  .navbar a:hover {
+    background-color: white !important;
+    color: #0ba6ff !important;
+  }
+
+  .navbar-item .is-active a {
+    background-color: white !important;
+    color: #0ba6ff !important;
+  }
+
+  .navbar-link {
+    color: black !important;
+  }
+
+  a.navbar-item.is-active:not(:focus):not(:hover) {
+    background-color: white !important;
+    color: #0ba6ff !important;
+  }
+
+  a.navbar-item.is-active, a.navbar-item:focus, a.navbar-item:focus-within, a.navbar-item:hover {
+    background-color: white !important;
+    color: #0ba6ff !important;
+  }
+
+  .navbar-link:not(.is-arrowless):after {
+    border-color: #0ba6ff !important;
+  }
+
+  .sidebar {
+    background-color: #00507d !important;
+  }
+
+  .sidebar .instance-summary {
+    background-color: #0ba6ff !important;
+  }
+
+  /*.sidebar .instance-summary--UP {
+    background-color: #0ba6ff !important;
+  }*/
+  .is-active {
+    color: white !important;
+  }
+  .sidebar .sidebar-group.is-active .sidebar-group-items a.is-active {
+    background-color: #0078BD !important;
+  }
+
+  .sidebar .sidebar-group.is-active .sidebar-group-items a:hover {
+    background-color: #0ba6ff !important;
+  }
+
+  .sidebar .sidebar-group.is-active {
+    box-shadow: inset 4px 0 0 white !important;
+  }
+
+  .sidebar a {
+    color: white !important;
+  }
+
+  .sidebar a:hover {
+    color: white !important;
+  }
+
+  .tag:not(body).is-primary {
+    background-color: #0ba6ff !important;
+  }
+
+  .hex.is-primary polygon {
+    fill: #0078bd !important;
+    stroke: #0078bd !important;
+  }
+
+  .button.is-primary {
+    background-color: #0078BD !important;
+  }
+
+  .button .is-primary :hover {
+    background-color: #0ba6ff !important;
+  }
+
+  .health-details__status--UP {
+    color: #0078bd !important;
+  }
+
+  .application-status__icon--UP {
+    color: #0078bd !important;
+  }
+
+  .has-text-success {
+    color: #0078bd !important;
+  }
+
+  .login {
+    background-color: #880000 !important;
+    /*background-image: url("/assets/img/background.png") !important;*/
+  }
+
+  .hero.is-primary {
+    background-color: #0078bd !important;
+  }
+
+  .has-text-primary {
+    background-color: #0078bd !important;
+  }
+
+  a {
+    color: #0078bd !important;
+  }
+
+  a:hover {
+    color: #0ba6ff !important;
   }
 </style>
 
